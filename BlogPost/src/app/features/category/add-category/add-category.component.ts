7f94b40f75a1bb5c9acd44cff09beabebdc32aed
sampat1 <sampat.adhikari@gmail.com>
2025-02-04 09:08:40 +1000

    added angular project and list,add component

---
+++ BlogPost/src/app/features/category/add-category/add-category.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { AddCategoryRequest } from '../models/add-category-request.model';
+import { CategoryService } from '../services/category.service';
+
+@Component({
+  selector: 'app-add-category',
+  templateUrl: './add-category.component.html',
+  styleUrls: ['./add-category.component.css']
+})
+export class AddCategoryComponent {
+model:AddCategoryRequest;
+constructor(private categoryService:CategoryService ){
+  this.model={
+    name:'',
+    urlHandle:''
+  }
+}
+  onCategoryAdd(){
+ 
+    this.categoryService.addCategory(this.model)
+    .subscribe({
+      next:(Response)=>{
+console.log('success');
+      },
+      // error:(erro)=>{
+
+      // }
+    });
+  }
+}
