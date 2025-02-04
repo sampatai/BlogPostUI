a92481faccc4bcf02dd8e7ecb3fd5df9d8eebf17
sampat1 <sampat.adhikari@gmail.com>
2025-02-04 10:08:45 +1000

    completed category  add

--- BlogPost/src/app/features/category/add-category/add-category.component.ts
+++ BlogPost/src/app/features/category/add-category/add-category.component.ts
@@ -1,30 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { CategoryService } from '../services/category.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-category',
   templateUrl: './add-category.component.html',
-  styleUrls: ['./add-category.component.css']
+  styleUrls: ['./add-category.component.css'],
 })
-export class AddCategoryComponent {
-model:AddCategoryRequest;
-constructor(private categoryService:CategoryService ){
-  this.model={
-    name:'',
-    urlHandle:''
+export class AddCategoryComponent implements OnDestroy {
+  model: AddCategoryRequest;
+  private addCategorySubscription?: Subscription;
+  constructor(private categoryService: CategoryService) {
+    this.model = {
+      name: '',
+      urlHandle: '',
+    };
   }
-}
-  onCategoryAdd(){
- 
-    this.categoryService.addCategory(this.model)
-    .subscribe({
-      next:(Response)=>{
-console.log('success');
+
+  onCategoryAdd() {
+   this.addCategorySubscription= this.categoryService.addCategory(this.model).subscribe({
+      next: (Response) => {
+        console.log('success');
       },
       // error:(erro)=>{
 
       // }
     });
+  }
+  ngOnDestroy(): void {
+    this.addCategorySubscription?.unsubscribe();
   }
 }
